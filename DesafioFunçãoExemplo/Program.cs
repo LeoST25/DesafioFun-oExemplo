adf035d77ef56ca862cbe93f338d6fd3a6aaf905
LeoST25 <matheusleonardo224@gmail.com>
2024-05-29 04:39:24 -0300

    adicionando arquivo csproj e arquivo de classe

---
+++ DesafioFunçãoExemplo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program {
+    static int CalcularDano(int ataque, int defesa) {
+        //TODO: Implemente a função calcular dano
+        int dano = ataque - defesa;
+
+        if (dano < 0) {
+            dano = 0;
+        }
+        return dano;
+    }
+
+    static void Main(string[] args) {
+        Console.WriteLine("Digite o ataque do inimigo:");
+        int ataque = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a sua defesa:");
+        int defesa = int.Parse(Console.ReadLine());
+
+        int danoCausado = CalcularDano(ataque, defesa);
+        Console.WriteLine("O dano causado pelo ataque foi: " + danoCausado);
+    }
+}
